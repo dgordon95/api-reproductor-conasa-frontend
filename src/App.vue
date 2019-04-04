527b427e5351a992a6f52f07bdb11d3e350f61db
Daniel Gordon Muñoz <daniel.gordon@gtei.es>
2019-04-04 11:12:22 +0200

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      <Navbar/>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  data() {
+    return {
+      message: 'Hello World',
+    };
+  },
+  components: {
+    'Navbar': Navbar,
+  }
+};
+</script>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
